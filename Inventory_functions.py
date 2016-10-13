3e975d1d9a95090662f66872c76c6cd743e581ab
Narendra Annabathula <narendra.33@gmail.com>
2016-10-12 19:40:15 -0700

    added function get_disks_info()

--- Inventory_functions.py
+++ Inventory_functions.py
@@ -23,12 +23,16 @@ def kb_to_gb(val_in_kb):
   return str(val_in_gb) + 'GB'
 
 
-partions = psutil.disk_partitions()
-dev_to_mnt_point_dict = {}
-dev_to_disk_space = {}
-devices = []
-mount_points = []
-output = {}
+
+
+
+
+# partions = psutil.disk_partitions()
+# dev_to_mnt_point_dict = {}
+# dev_to_disk_space = {}
+# devices = []
+# mount_points = []
+# output = {}
 
 # for partion in partions:
 #   #print partion.device
@@ -48,6 +52,29 @@ output = {}
 #   dev_to_disk_space['DeviceID: '+dev] = disk_stats
 
 
+
+def get_disks_info():
+  partions = psutil.disk_partitions()
+  dev_to_mnt_point_dict = {}
+  dev_to_disk_space = {}
+  devices = []
+  mount_points = []
+  output = {}
+  for partion in partions:
+    dev_to_mnt_point_dict[partion.device] = partion.mountpoint
+  for dev, mnt_point in dev_to_mnt_point_dict.iteritems():
+    usage = psutil.disk_usage(mnt_point)
+    total_disk = usage.total
+    usage_percent = usage.percent
+    mnt_point_dict = {}
+    disk_dict = {}
+    used_per_dict = {}
+    mnt_point_dict['mount_point'] = mnt_point  
+    disk_dict['Total_disk'] = total_disk
+    used_per_dict['Percent_used'] = usage_percent
+    disk_stats = [mnt_point_dict,disk_dict,used_per_dict]
+    dev_to_disk_space['DeviceID: '+dev] = disk_stats
+  return dev_to_disk_space
 
 
 #1) for getting DNS Domain Name 
