5292ed8d405e15c99f23e7dd5c515abb888db456
Narendra Annabathula <narendra.33@gmail.com>
2016-10-12 20:14:07 -0700

    made by return to list of softwares

--- softwares.py
+++ softwares.py
@@ -12,6 +12,7 @@ software_to_vendor = {}
 software_to_description = {}
 software_to_version_vendor = {}
 softwares = []
+return_software_list = []
 for i in list_of_sw.split('\n'):
      tmp_list = i.split()
      if len(tmp_list) == 3:
@@ -21,8 +22,15 @@ for i in list_of_sw.split('\n'):
 
 for software in softwares:
          version_vendor_list = []
-         version_vendor_list = [{'version':software_to_version[software]},{'vendor':software_to_vendor[software]}]
-         software_to_version_vendor['product_name: ' +  software]=version_vendor_list
+         software_details_dict = {}
+         # version_vendor_list = [{'version':software_to_version[software]},{'vendor':software_to_vendor[software]}]
+         # software_to_version_vendor['product_name: ' +  software]=version_vendor_list
+         software_details_dict{'ProductName:'} = software
+         software_details_dict{'Version:'} = software_to_version[software]
+         software_details_dict{'Vendor'} = software_to_vendor[software]
+         return_software_list.append(software_details_dict)
 
-pprint(software_to_version_vendor)
+
+
+pprint(return_software_list)
 
