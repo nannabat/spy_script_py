dbdb8b961f5b94c9a5de082ac4572d62d4b1e8d2
Narendra Annabathula <narendra.33@gmail.com>
2016-10-12 21:44:36 -0700

    made changes Inventory_fetch to print

--- Inventory_fetch.py
+++ Inventory_fetch.py
@@ -2,12 +2,21 @@ import Inventory_functions
 from pprint import pprint
 Inventory_list = ['Disks','DNSHostName','Domain','Softwares']
 Inventory_list.sort()
+value_set = {}
 return_dict_all_values = {}
-Disks = Inventory_functions.get_disks_info()
-Softwares = Inventory_functions.get_softwares_info()
-Domain = Inventory_functions.get_dna()
-DNSHostName = Inventory_functions.get_dom()
+#Disks = Inventory_functions.get_disks_info()
+value_set['Disks'] = Inventory_functions.get_disks_info()
+#Softwares = Inventory_functions.get_softwares_info()
+value_set['Softwares'] = Inventory_functions.get_softwares_info()
+#pprint(Softwares)
+#Domain = Inventory_functions.get_dna()
+#pprint(Domain)
+value_set['Domain'] = Inventory_functions.get_dna()
+#DNSHostName = Inventory_functions.get_dom()
+#pprint(DNSHostName)
+value_set['DNSHostName'] = Inventory_functions.get_dom()
 for value in Inventory_list:
-	return_dict_all_values{value} = value
+        Disks = Inventory_functions.get_disks_info()
+	return_dict_all_values[value] = value_set[value]
 pprint(return_dict_all_values)
 
