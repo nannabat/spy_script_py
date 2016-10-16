8684694050b5311152694e69ecec32483162da20
RajeshPulabala225 <RajeshPulabala@silicus.com>
2016-10-16 16:56:57 -0500

    adding Network-usage.py script for task3

--- Inventory_fetch.py
+++ Inventory_fetch.py
@@ -1,22 +1,21 @@
 import Inventory_functions
 from pprint import pprint
-Inventory_list = ['Disks','DNSHostName','Domain','Softwares']
+Inventory_list = ['Disks','DNSHostName','Domain','ReportDateTime','Manufacturer','Model','NumberOfLogicalProcessors','NumberOfProcessors','Softwares']
 Inventory_list.sort()
 value_set = {}
 return_dict_all_values = {}
-#Disks = Inventory_functions.get_disks_info()
+
 value_set['Disks'] = Inventory_functions.get_disks_info()
-#Softwares = Inventory_functions.get_softwares_info()
 value_set['Softwares'] = Inventory_functions.get_softwares_info()
-#pprint(Softwares)
-#Domain = Inventory_functions.get_dna()
-#pprint(Domain)
+value_set['ReportDateTime'] = Inventory_functions.get_tm()
 value_set['Domain'] = Inventory_functions.get_dna()
-#DNSHostName = Inventory_functions.get_dom()
-#pprint(DNSHostName)
+value_set['Manufacturer'] = Inventory_functions.get_mani()
+value_set['Model'] = Inventory_functions.get_mod()
+value_set['NumberOfLogicalProcessors'] = Inventory_functions.get_proc1()
+value_set['NumberOfProcessors'] = Inventory_functions.get_proc2()
+
 value_set['DNSHostName'] = Inventory_functions.get_dom()
 for value in Inventory_list:
         Disks = Inventory_functions.get_disks_info()
 	return_dict_all_values[value] = value_set[value]
 pprint(return_dict_all_values)
-
