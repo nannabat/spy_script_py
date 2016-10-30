132e2d113170434b31ca1cd4be295d07be1b7998
Narendra Annabathula <narendra.33@gmail.com>
2016-10-29 18:47:35 -0700

    changed names of Rx, Tx, ethernet

--- Network-Usage.py
+++ Network-Usage.py
@@ -58,8 +58,9 @@ def nics_report():
     dict_nics_info = {}
 
     outer_nic_list_with_conf = nics_with_config()
+    outer_nic_list_with_conf.remove('lo')
 
-    for new_nic in outer_nic_list_with_conf.remove('lo'):
+    for new_nic in outer_nic_list_with_conf:
         #new_correct_nic = new_nic.replace(':',"")
         #print new_correct_nic
         new_correct_nic = new_nic
@@ -71,6 +72,7 @@ def nics_report():
         output_ifconfig_list = output_ifconfig.split('\n\n')
 
         mac_rx_tx = []
+        dhcp_stat_dict = {}
 
         for output_line in output_ifconfig_list:
             nic_line_list = output_line.splitlines()
@@ -86,21 +88,23 @@ def nics_report():
                 if 'ether' in line:
                     dict_ether_mac = {}
                     ether_mac_line = line.split()
-                    print ether_mac_line
-                    dict_ether_mac[ether_mac_line[0]] = str(ether_mac_line[1])
+                    #print ether_mac_line
+                    #dict_ether_mac[ether_mac_line[0]] = str(ether_mac_line[1])
+                    dict_ether_mac['MAC Address'] = str(ether_mac_line[1])
                     mac_rx_tx.append(dict_ether_mac)
                 if 'RX' in line and ('packets' in line):
                     rx_bytes = {}
                     rx_bytes_line = line.split()
-                    rx_bytes[rx_bytes_line[0]+'_'+rx_bytes_line[3]] = rx_bytes_line[4]
+                    rx_bytes['Recevied Bytes'] = str(rx_bytes_line[4]) + ' ' + 'bytes'
                     mac_rx_tx.append(rx_bytes)
                 if 'TX' in line and ('packets' in line):
                     tx_bytes = {}
                     tx_bytes_line = line.split()
-                    tx_bytes[tx_bytes_line[0]+'_'+tx_bytes_line[3]] = tx_bytes_line[4]
+                    tx_bytes['Transmitted Bytes'] = tx_bytes_line[4] + ' ' + 'bytes'
                     mac_rx_tx.append(tx_bytes)
-        mac_rx_tx.append(check_nic_conf_file(new_nic))
+        dhcp_stat_dict['DHCP status'] = check_nic_conf_file(new_nic)
+        mac_rx_tx.append(dhcp_stat_dict)
         dict_nics_info[new_correct_nic] = mac_rx_tx
     return dict_nics_info
 
-print json.dumps(nics_report,indent = 4)
+print json.dumps(nics_report(),indent = 4)
