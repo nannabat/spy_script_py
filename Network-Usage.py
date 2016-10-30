26ce44540508859292825994749602538b57e0e5
Narendra Annabathula <narendra.33@gmail.com>
2016-10-29 17:43:09 -0700

    converted all to functions and removed lo:

--- Network-Usage.py
+++ Network-Usage.py
@@ -8,7 +8,7 @@ import struct
 import os
 
 
-dict_nics_info = {}
+
 
 
 def get_nics():
@@ -31,10 +31,13 @@ def check_nic_conf_file(calling_nic):
     path_config = "/etc/sysconfig/network-scripts/" + "ifcfg-" + calling_nic
     conf_file = open(path_config,'r')
     config = conf_file.read()
-    print config
+    #print config
     return_value = config.rfind('BOOTPROTO=dhcp')
     if return_value != -1:
-        print 'DHCP enabled for ' + calling_nic            
+        return 'Dhcp enabled'
+
+    else:
+        return 'Dhcp disabled'          
 
 
 
@@ -50,52 +53,58 @@ def check_nic_conf_file(calling_nic):
 # print new_nic_list
 # check_nic_conf_file()
 
-outer_nic_list_with_conf = nics_with_config()
+def nics_report():
 
-for new_nic in outer_nic_list_with_conf:
-    #new_correct_nic = new_nic.replace(':',"")
-    #print new_correct_nic
-    new_correct_nic = new_nic
-    check_nic_conf_file(new_correct_nic)
-    cmd = 'ifconfig ' + new_correct_nic
+    dict_nics_info = {}
 
-    output_ifconfig = subprocess.check_output(cmd,shell=True)
+    outer_nic_list_with_conf = nics_with_config()
 
-    output_ifconfig_list = output_ifconfig.split('\n\n')
+    for new_nic in outer_nic_list_with_conf.remove('lo'):
+        #new_correct_nic = new_nic.replace(':',"")
+        #print new_correct_nic
+        new_correct_nic = new_nic
+        check_nic_conf_file(new_correct_nic)
+        cmd = 'ifconfig ' + new_correct_nic
 
-    mac_rx_tx = []
+        output_ifconfig = subprocess.check_output(cmd,shell=True)
 
-    for output_line in output_ifconfig_list:
-            nic_line_list = output_line.splitlines()
-            for line in nic_line_list:
-                    #print line
-                    if 'inet' in line and ('netmask' in line):
-                    	dict_inet_nmsk_broadcast = {}
-                        list_inet_line = line.split()
-                        dict_inet_nmsk_broadcast[list_inet_line[0]] = str(list_inet_line[1])
-                        dict_inet_nmsk_broadcast[list_inet_line[2]] = str(list_inet_line[3])
-                        if 'inet' in line and ('netmask' in line) and  ('broadcast' in line):
-                             dict_inet_nmsk_broadcast[list_inet_line[4]] = str(list_inet_line[5])
-                        mac_rx_tx.append(dict_inet_nmsk_broadcast)
-                    if 'ether' in line:
-                        dict_ether_mac = {}
-                        ether_mac_line = line.split()
-                        dict_ether_mac[ether_mac_line[0]] = str(ether_mac_line[1])
-                        mac_rx_tx.append(dict_ether_mac)
+        output_ifconfig_list = output_ifconfig.split('\n\n')
+
+        mac_rx_tx = []
+
+        for output_line in output_ifconfig_list:
+                nic_line_list = output_line.splitlines()
+                for line in nic_line_list:
+                        #print line
+                        if 'inet' in line and ('netmask' in line):
+                        	dict_inet_nmsk_broadcast = {}
+                            list_inet_line = line.split()
+                            dict_inet_nmsk_broadcast[list_inet_line[0]] = str(list_inet_line[1])
+                            dict_inet_nmsk_broadcast[list_inet_line[2]] = str(list_inet_line[3])
+                            if 'inet' in line and ('netmask' in line) and  ('broadcast' in line):
+                                 dict_inet_nmsk_broadcast[list_inet_line[4]] = str(list_inet_line[5])
+                            mac_rx_tx.append(dict_inet_nmsk_broadcast)
+                        if 'ether' in line:
+                            dict_ether_mac = {}
+                            ether_mac_line = line.split()
+                            dict_ether_mac[ether_mac_line[0]] = str(ether_mac_line[1])
+                            mac_rx_tx.append(dict_ether_mac)
 
 
-                    if 'RX' in line and ('packets' in line):
-                            #print line
-                        rx_bytes = {}
-                        rx_bytes_line = line.split()
-                        rx_bytes[rx_bytes_line[0]+'_'+rx_bytes_line[3]] = rx_bytes_line[4]
-                        mac_rx_tx.append(rx_bytes)
-                    if 'TX' in line and ('packets' in line):
-                            #print line
-                        tx_bytes = {}
-                        tx_bytes_line = line.split()
-                        tx_bytes[tx_bytes_line[0]+'_'+tx_bytes_line[3]] = tx_bytes_line[4]
-                        mac_rx_tx.append(tx_bytes)
-    #print mac_rx_tx
-    dict_nics_info[new_correct_nic] = mac_rx_tx
-print json.dumps(dict_nics_info,indent = 4)
+                        if 'RX' in line and ('packets' in line):
+                                #print line
+                            rx_bytes = {}
+                            rx_bytes_line = line.split()
+                            rx_bytes[rx_bytes_line[0]+'_'+rx_bytes_line[3]] = rx_bytes_line[4]
+                            mac_rx_tx.append(rx_bytes)
+                        if 'TX' in line and ('packets' in line):
+                                #print line
+                            tx_bytes = {}
+                            tx_bytes_line = line.split()
+                            tx_bytes[tx_bytes_line[0]+'_'+tx_bytes_line[3]] = tx_bytes_line[4]
+                            mac_rx_tx.append(tx_bytes)
+        mac_rx_tx.append(check_nic_conf_file(new_nic))
+        dict_nics_info[new_correct_nic] = mac_rx_tx
+    return dict_nics_info
+
+print json.dumps(nics_report,indent = 4)
