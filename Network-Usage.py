da4762ed1a3b7ec51bfc8f697159ad374cc47b19
Narendra Annabathula <narendra.33@gmail.com>
2016-10-29 18:18:38 -0700

    corrected indentation fof if statements

--- Network-Usage.py
+++ Network-Usage.py
@@ -80,24 +80,24 @@ def nics_report():
                     list_inet_line = line.split()
                     dict_inet_nmsk_broadcast[list_inet_line[0]] = str(list_inet_line[1])
                     dict_inet_nmsk_broadcast[list_inet_line[2]] = str(list_inet_line[3])
-                    if 'inet' in line and ('netmask' in line) and  ('broadcast' in line):
-                        dict_inet_nmsk_broadcast[list_inet_line[4]] = str(list_inet_line[5])
-                        mac_rx_tx.append(dict_inet_nmsk_broadcast)
-                        if 'ether' in line:
-                            dict_ether_mac = {}
-                            ether_mac_line = line.split()
-                            dict_ether_mac[ether_mac_line[0]] = str(ether_mac_line[1])
-                            mac_rx_tx.append(dict_ether_mac)
-                        if 'RX' in line and ('packets' in line):
-                            rx_bytes = {}
-                            rx_bytes_line = line.split()
-                            rx_bytes[rx_bytes_line[0]+'_'+rx_bytes_line[3]] = rx_bytes_line[4]
-                            mac_rx_tx.append(rx_bytes)
-                        if 'TX' in line and ('packets' in line):
-                            tx_bytes = {}
-                            tx_bytes_line = line.split()
-                            tx_bytes[tx_bytes_line[0]+'_'+tx_bytes_line[3]] = tx_bytes_line[4]
-                            mac_rx_tx.append(tx_bytes)
+                if 'inet' in line and ('netmask' in line) and  ('broadcast' in line):
+                    dict_inet_nmsk_broadcast[list_inet_line[4]] = str(list_inet_line[5])
+                    mac_rx_tx.append(dict_inet_nmsk_broadcast)
+                if 'ether' in line:
+                    dict_ether_mac = {}
+                    ether_mac_line = line.split()
+                    dict_ether_mac[ether_mac_line[0]] = str(ether_mac_line[1])
+                    mac_rx_tx.append(dict_ether_mac)
+                if 'RX' in line and ('packets' in line):
+                    rx_bytes = {}
+                    rx_bytes_line = line.split()
+                    rx_bytes[rx_bytes_line[0]+'_'+rx_bytes_line[3]] = rx_bytes_line[4]
+                    mac_rx_tx.append(rx_bytes)
+                if 'TX' in line and ('packets' in line):
+                    tx_bytes = {}
+                    tx_bytes_line = line.split()
+                    tx_bytes[tx_bytes_line[0]+'_'+tx_bytes_line[3]] = tx_bytes_line[4]
+                    mac_rx_tx.append(tx_bytes)
         mac_rx_tx.append(check_nic_conf_file(new_nic))
         dict_nics_info[new_correct_nic] = mac_rx_tx
     return dict_nics_info
