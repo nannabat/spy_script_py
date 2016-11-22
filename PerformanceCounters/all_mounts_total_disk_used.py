d48a24a00c4ba57f64efc6e9efa2fd13cc89d9e5
RajeshPulabala225 <RajeshPulabala@silicus.com>
2016-11-21 23:06:42 -0600

    latest changes on performance counters at instance level

--- PerformanceCounters/all_mounts_total_disk_used.py
+++ PerformanceCounters/all_mounts_total_disk_used.py
@@ -51,6 +51,17 @@ def get_readbytes_with_timestamp():
         ts_return_list.append(ts_st)
         ts_return_list.append(ts_d)
         return ts_return_list
+def get_readbytes_with_timestamp_dynamic(mount_point):
+        ts_return_list = []
+        ts_a = psutil.disk_usage(mount_point)
+        ts_b = ts_a.used
+        ts_d = (ts_b/(1024*1024))
+        ts_time_stamp = time.time()
+        ts_st = datetime.datetime.fromtimestamp(ts_time_stamp).strftime('%Y-%m-%d %H:%M:%S')
+        ts_return_list.append(ts_st)
+        ts_return_list.append(ts_d)
+        return ts_return_list
+
 def get_readbytes():
         #return_list = []
         a = psutil.disk_usage('/')
@@ -75,7 +86,9 @@ dd = "Avg_Total_MegaBytes_Used"
 ee = "20sec"
 all = {'Duration: ':ee,'PerformanceCounterCategory: ':aa,'PerformanceCounterInstanceName: ':bb,'PerformanceCounterLabel: ':cc,'PerformanceCounterName: ':dd,'AverageValue: ':x,'MaxValue: ':y,'MinValue: ':z,'RecordCount: ':RecordCount,'HostName: ':var1,'ReportDateTime: ':var100,'IPAddress: ':ip_Address}
 
-print get_mount_and_device()
+print get_mount_and_device().keys()
+
+
 
 for k,v in sorted(all.items()):
 	print k, v
