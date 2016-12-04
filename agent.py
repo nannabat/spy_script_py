95fe2c921fc856cc434b285057062a4782c2fa00
narendraannabathula <narendra.33@gmail.com>
2016-12-04 12:40:52 -0800

    Added variable ABS_PATH_AGENT_CONF_FILE

--- agent.py
+++ agent.py
@@ -1,13 +1,15 @@
 #!/usr/bin/python
 import pyinotify,subprocess
 from crontab import CronTab
+
+ABS_PATH_AGENT_CONF_FILE='/root/spy_script_py/agentconfig.json'
 def onChange(ev):
-    cmd = ['/bin/echo', '/root/spy_script_py/agentconfig.json', ev.pathname, 'changed']
+    cmd = ['/bin/echo',ABS_PATH_AGENT_CONF_FILE, ev.pathname, 'changed']
     subprocess.Popen(cmd).communicate()
 
 
 if __name__ == '__main__':
 	wm = pyinotify.WatchManager()
-	wm.add_watch('/root/spy_script_py/agentconfig.json', pyinotify.IN_MODIFY, onChange)
+	wm.add_watch(ABS_PATH_AGENT_CONF_FILE, pyinotify.IN_MODIFY, onChange)
 	notifier = pyinotify.Notifier(wm)
 	notifier.loop()
