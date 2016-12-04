bbc9dc1a0f16950ac2594ab2c8ca5d8279022c24
Narendra Annabathula <narendra.33@gmail.com>
2016-12-04 13:23:41 -0800

    added method to get intial values and print them

--- agent.py
+++ agent.py
@@ -16,16 +16,25 @@ def onChange(ev):
     	configurations = json.load(config_file)
     	for scriptname,time_value in configurations.items():
     		print 'for ' + scriptname + ' the time value set is: ' +  time_value
+
+def get_intial_values_list():
+	config_file = open(ABS_PATH_AGENT_CONF_FILE,'r')
+	configurations = json.load(config_file)
+	return_dict_intial_values = {}
+	for scriptname,time_value in configurations.items():
+		return_dict_intial_values[scriptname] = time_value
+	return return_dict_intial_values
+		
+
+
+
     		
 if __name__ == '__main__':
 	wm = pyinotify.WatchManager()
 	wm.add_watch(ABS_PATH_AGENT_CONF_FILE, pyinotify.IN_MODIFY, onChange)
 	notifier = pyinotify.Notifier(wm)
 	print "the value of CONF_MODIFIED: " + str(CONF_MODIFIED)
+	print get_intial_values_list
 	notifier.loop()
-	# if CONF_MODIFIED == True:
-	# 	config_file = open('agentconfig.json','r')
-	# 	configurations = json.load(config_file)
-	# 	for scriptname,time_value in configurations.items():
-	# 		print 'for ' + scriptname + ' the time value set is: ' +  time_value
+
 		
